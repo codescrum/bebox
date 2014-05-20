b639583851a835fa4034c79c0711de1ab5253147
Juan Pablo Amaya <jp.amaya@codescrum.com>
2014-05-20 16:57:00 -0500

    Refactor Server spec

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,41 +1,33 @@
 require 'spec_helper'
 
 describe Bebox::Server do
-  subject{Bebox::Server.new(ip: @ip, hostname: @hostname)}
-  #let(:ip){'192.168.0.10'}
-  #let(:hostname){'server1.projectname.test'}
+
+  subject { build(:server) }
 
   describe '#ip' do
     it 'returns the ip' do
-      @ip = '192.168.0.10'
-      expect(subject.ip).to eq('192.168.0.10')
+      expect(subject.ip).to eq('192.168.0.70')
     end
   end
 
   describe '#hostname' do
     its 'returns the hostname' do
-      @hostname = 'server1.projectname.test'
-      expect(subject.hostname).to eq('server1.projectname.test')
+      expect(subject.hostname).to eq('server1.pname.test')
     end
   end
 
-  context 'check it out if the input are valid' do
+  context 'check it out if the input is valid' do
     it 'returns true because the ip is free' do
-      @ip = '192.168.0.54'
       expect(subject.ip_free?).to eq(true)
     end
 
     it 'returns false because the ip is not free' do
-      @ip = '127.0.0.1'
+      subject.ip = '127.0.0.1'
       expect(subject.ip_free?).to eq(false)
     end
 
-    it 'returns true if the hostname' do
-     #
-    end
-
     it 'should raise ip is already taken error' do
-      @ip = '127.0.0.1'
+      subject.ip = '127.0.0.1'
       subject.ip_free?
       expect(subject.errors.full_messages.first).to eq("Ip is already taken!")
     end
