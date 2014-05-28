e102231a9088a1f7dbc9d98245b0c36d786cc591
Juan Pablo Amaya <jp.amaya@codescrum.com>
2014-05-28 11:18:45 -0500

    Add common modules nginx

--- lib/bebox/puppet.rb
+++ lib/bebox/puppet.rb
@@ -20,5 +20,40 @@ module Bebox
     def deploy
       `cd #{self.environment.project.path} && BUNDLE_GEMFILE=Gemfile bundle exec cap #{self.environment.name} deploy:prepare_puppet_user -s phase='deploy_puppet_user'`
     end
+
+    def configure_common_modules
+      copy_modules
+      setup_manifest
+      setup_common_hiera
+      deploy
+      apply_common_modules
+    end
+
+    def copy_modules
+      `cp -r lib/modules/* #{self.environment.project.path}/puppet/modules`
+    end
+
+    def setup_manifest
+      manifest_template = Tilt::ERBTemplate.new("templates/puppet/manifests/site.pp.erb")
+      File.open("#{self.environment.project.path}/puppet/manifests/site.pp", 'w') do |f|
+        f.write manifest_template.render(self)
+      end
+    end
+
+    def setup_common_hiera
+      hiera_template = Tilt::ERBTemplate.new("templates/puppet/hiera/hiera.yaml.erb")
+      File.open("#{self.environment.project.path}/puppet/hiera/hiera.yaml", 'w') do |f|
+        f.write hiera_template.render(self)
+      end
+      common_hiera_template = Tilt::ERBTemplate.new("templates/puppet/hiera/data/common.yaml.erb")
+      File.open("#{self.environment.project.path}/puppet/hiera/data/common.yaml", 'w') do |f|
+        f.write common_hiera_template.render(self)
+      end
+    end
+
+    def apply_common_modules
+      `cd #{self.environment.project.path} && BUNDLE_GEMFILE=Gemfile bundle exec cap #{self.environment.name} puppet:apply -s phase='apply_modules'`
+    end
+
   end
 end
