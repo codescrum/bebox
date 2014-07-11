b5468018f3d54a28d622368bdf30e1e4b7fd79eb
Juan Pablo Amaya <jp.amaya@codescrum.com>
2014-07-11 15:19:17 -0500

    Add ssh_forwarding rules to puppet step-1

--- lib/templates/puppet/default_profiles/users/manifests/init.pp
+++ lib/templates/puppet/default_profiles/users/manifests/init.pp
@@ -11,4 +11,36 @@
 
 class profiles::users {
   include ::users
+
+  # Configure agent forwarding for application user
+  $ssh_forwarding_hash = hiera_hash('ssh::client_rules', {})
+  class { 'ssh::client':
+    storeconfigs_enabled => false,
+    options => $ssh_forwarding_hash
+  }
 }
+
+# define profiles::users::get_forwarding_rules (
+#   $ssh_forwarding_hash = {},
+#   ) {
+#   $forwarding_hosts = {}
+#   each($ssh_forwarding_hash) |$user, $user_forwarding_hash| {
+#     $hosts = $user_forwarding_hash[hosts]
+#     each($hosts) |$host, $value| {
+#       $host_hash = { "Host $host" => { 'User' => $user, 'ForwardAgent' => $value, 'StrictHostKeyChecking' => 'no' } }
+#       $forwarding_hosts += $host_hash
+#     }
+#   }
+#   profiles::users::forwarding { 'forwardinghosts':
+#     forwarding_hosts => $forwarding_hosts,
+#   }
+# }
+
+# define profiles::users::forwarding (
+#   $forwarding_hosts = {},
+#   ){
+#   class { 'ssh::client':
+#     storeconfigs_enabled => false,
+#     options => $forwarding_hosts
+#   }
+# }
