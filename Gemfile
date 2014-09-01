5853a69e393ec826c33a74d586019f1cb41294ac
Juan Pablo Amaya <jp.amaya@codescrum.com>
2014-09-01 18:40:11 -0500

    Add simple_cov for coverage

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source 'https://rubygems.org'
 
 gemspec
+
+gem 'simplecov', :require => false, :group => :test
+gem 'simplecov-badge', :require => false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,20 @@
+require 'simplecov'
+SimpleCov.start
+SimpleCov.start do
+    require 'simplecov-badge'
+    # add your normal SimpleCov configs
+    add_filter "/app/admin/"
+    # configure any options you want for SimpleCov::Formatter::BadgeFormatter
+    SimpleCov::Formatter::BadgeFormatter.generate_groups = true
+    SimpleCov::Formatter::BadgeFormatter.strength_foreground = true
+    SimpleCov::Formatter::BadgeFormatter.timestamp = true
+    # call SimpleCov::Formatter::BadgeFormatter after the normal HTMLFormatter
+    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+        SimpleCov::Formatter::HTMLFormatter,
+        SimpleCov::Formatter::BadgeFormatter,
+    ]
+end
+
 require 'rubygems'
 require 'awesome_print'
 require 'jazz_hands'
