a6d69894f93e429cb7e04609a817fbd57c2f1a16
Juan Pablo Amaya <jp.amaya@codescrum.com>
2014-09-08 12:40:23 -0500

    Fix role wizard test

--- spec/role_wizard_spec.rb
+++ spec/role_wizard_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 require_relative '../spec/factories/role.rb'
 require_relative '../spec/factories/profile.rb'
 
-describe 'Test 08: Bebox::RoleWizard' do
+describe 'Test 00: Bebox::RoleWizard' do
 
   subject { Bebox::RoleWizard.new }
 
@@ -14,31 +14,38 @@ describe 'Test 08: Bebox::RoleWizard' do
     $stdout.stub(:write)
   end
 
-  after :all do
-    role.remove
-    profile.remove
+  it 'checks if a role exists' do
+    output = subject.role_exists?(role.project_root, role.name)
+    expect(output).to eq(false)
   end
 
   context '00: role not exist' do
+
+    before :each do
+      subject.stub(:role_exists?) { false }
+    end
+
     it 'creates a new role with wizard' do
       Bebox::Role.any_instance.stub(:create) { true }
       output = subject.create_new_role(role.project_root, role.name)
       expect(output).to eq(true)
     end
-  end
 
-  it 'can not removes a role if not exist any role' do
-    Bebox::Role.stub(:list) {[]}
-    output = subject.remove_role(role.project_root)
-    expect(output).to eq(nil)
+    it 'can not removes a role if not exist any role' do
+      Bebox::Role.stub(:list) {[]}
+      output = subject.remove_role(role.project_root)
+      expect(output).to eq(nil)
+    end
   end
 
   context '01: role exist' do
-    before :all do
-      role.create
+
+    before :each do
+      subject.stub(:role_exists?) { true }
     end
 
     it 'removes a role with wizard' do
+      Bebox::Role.stub(:list) {[role.name]}
       Bebox::Role.any_instance.stub(:remove) { true }
       $stdin.stub(:gets).and_return('1', 'y')
       output = subject.remove_role(role.project_root)
@@ -47,11 +54,15 @@ describe 'Test 08: Bebox::RoleWizard' do
   end
 
   context '02: role and profile exist' do
-    before :all do
-      profile.create
+
+    before :each do
+      subject.stub(:role_exists?) { true }
+      Bebox::Profile.stub(:list) {[profile.relative_path]}
+      Bebox::Role.stub(:list) {[role.name]}
     end
 
     it 'adds a profile to a role with wizard' do
+      Bebox::Role.stub(:profile_in_role?) { false }
       Bebox::Role.stub(:add_profile) { true }
       $stdin.stub(:gets).and_return(role.name, profile.relative_path)
       output = subject.add_profile(role.project_root)
@@ -59,24 +70,23 @@ describe 'Test 08: Bebox::RoleWizard' do
     end
 
     context '03: profile included in a role' do
-      before :all do
-        Bebox::Role.add_profile(role.project_root, role.name, profile.relative_path)
-      end
 
       it 'adds a profile to a role with wizard' do
+        Bebox::Role.stub(:profile_in_role?) { true }
         $stdin.stub(:gets).and_return(role.name, profile.relative_path)
         output = subject.add_profile(role.project_root)
         expect(output).to eq(false)
       end
 
       it 'can not remove a profile that not exist in a role with wizard' do
-        Bebox::Role.stub(:profile_in_role?) {false}
+        Bebox::Role.stub(:profile_in_role?) { false }
         $stdin.stub(:gets).and_return(role.name, profile.relative_path)
         output = subject.remove_profile(role.project_root)
         expect(output).to eq(false)
       end
 
       it 'removes a profile from a role with wizard' do
+        Bebox::Role.stub(:profile_in_role?) { true }
         Bebox::Role.stub(:remove_profile) { true }
         $stdin.stub(:gets).and_return(role.name, profile.relative_path)
         output = subject.remove_profile(role.project_root)
