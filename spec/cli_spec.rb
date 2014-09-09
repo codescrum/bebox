21de5c2c420d1d1869a1736446097380260290cd
Juan Pablo Amaya <jp.amaya@codescrum.com>
2014-09-09 12:20:40 -0500

    Add cli commands tests for prepare and profile

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
+require_relative 'factories/environment.rb'
 require_relative 'factories/node.rb'
+require_relative 'factories/profile.rb'
 
 describe 'Test 00: Bebox::Cli' do
+
+  let(:environment) { build(:environment) }
+  let(:node) { build(:node) }
+  let(:profile) { build(:profile) }
 
   before :each do
     # $stderr.stub(:write)
@@ -10,7 +16,7 @@ describe 'Test 00: Bebox::Cli' do
   it 'shows the help for general commands' do
     argv = []
     output = capture(:stdout) { cli_command(argv, :success) }
-    expected_content = File.read("spec/fixtures/commands/general/help.test").gsub(/\s+/, ' ').strip
+    expected_content = File.read("spec/fixtures/commands/general_help.test").gsub(/\s+/, ' ').strip
     expect(output.gsub(/\s+/, ' ').strip).to eq(expected_content)
   end
 
@@ -38,7 +44,7 @@ describe 'Test 00: Bebox::Cli' do
     it 'shows the help for project commands' do
       argv = []
       output = capture(:stdout) { cli_command(argv, :success) }
-      expected_content = File.read("spec/fixtures/commands/project/help.test").gsub(/\s+/, ' ').strip
+      expected_content = File.read("spec/fixtures/commands/in_project_help.test").gsub(/\s+/, ' ').strip
       expect(output.gsub(/\s+/, ' ').strip).to eq(expected_content)
     end
 
@@ -47,15 +53,22 @@ describe 'Test 00: Bebox::Cli' do
       it 'shows the help for environment commands' do
         argv = ['help', 'environment']
         output = capture(:stdout) { cli_command(argv, :success) }
-        expected_content = File.read("spec/fixtures/commands/project/environment/help.test").gsub(/\s+/, ' ').strip
+        expected_content = File.read("spec/fixtures/commands/environment_help.test").gsub(/\s+/, ' ').strip
         expect(output.gsub(/\s+/, ' ').strip).to eq(expected_content)
       end
 
       it 'list environments' do
-        Bebox::Environment.stub(:list) { ['a', 'b', 'c'] }
+        Bebox::Environment.stub(:list) { [environment.name] }
         argv = ['environment', 'list']
         output = capture(:stdout) { cli_command(argv, :success) }
-        expect(output).to match(/Current environments:.*?a.*?b.*?c/im)
+        expect(output).to match(/Current environments:.*?#{environment.name}/im)
+      end
+
+      it 'not list environments if there are not any' do
+        Bebox::Environment.stub(:list) { [] }
+        argv = ['environment', 'list']
+        output = capture(:stdout) { cli_command(argv, :success) }
+        expect(output).to match(/Current environments:.*?There are not environments yet. You can create a new one with: 'bebox environment new' command./im)
       end
 
       it 'fails to create a new environment without name' do
@@ -67,7 +80,7 @@ describe 'Test 00: Bebox::Cli' do
 
       it 'creates a new environment with name' do
         Bebox::EnvironmentWizard.any_instance.stub(:send) { true }
-        argv = ['environment', 'new', 'a']
+        argv = ['environment', 'new', environment.name]
         capture(:stdout) { cli_command(argv, :success) }
       end
 
@@ -80,14 +93,12 @@ describe 'Test 00: Bebox::Cli' do
 
       it 'removes an environment with name' do
         Bebox::EnvironmentWizard.any_instance.stub(:send) { true }
-        argv = ['environment', 'remove', 'a']
+        argv = ['environment', 'remove', environment.name]
         capture(:stdout) { cli_command(argv, :success) }
       end
     end
 
     context '03: node commands' do
-
-      let(:node) { build(:node) }
 
       before :each do
         Bebox::Environment.stub(:list) { [node.environment] }
@@ -96,15 +107,31 @@ describe 'Test 00: Bebox::Cli' do
       it 'shows the help for node commands' do
         argv = ['help', 'node']
         output = capture(:stdout) { cli_command(argv, :success) }
-        expected_content = File.read("spec/fixtures/commands/project/node/help.test").gsub(/\s+/, ' ').strip
+        expected_content = File.read("spec/fixtures/commands/node_help.test").gsub(/\s+/, ' ').strip
         expect(output.gsub(/\s+/, ' ').strip).to eq(expected_content)
       end
 
-      it 'list nodes' do
+      it 'list nodes if there are any' do
         Bebox::Node.stub(:list) { [node.hostname] }
         argv = ['node', 'list']
         output = capture(:stdout) { cli_command(argv, :success) }
         expect(output).to match(/Nodes for '#{node.environment}' environment:.*?#{node.hostname}/m)
+      end
+
+      it 'not list nodes if there are not any' do
+        Bebox::Node.stub(:list) { [] }
+        argv = ['node', 'list']
+        output = capture(:stdout) { cli_command(argv, :success) }
+        expect(output).to match(/Nodes for '#{node.environment}' environment:.*?There are not nodes yet in the environment. You can create a new one with: 'bebox node new' command./m)
+      end
+
+      it 'sets a role for a node' do
+        Bebox::Profile.stub(:profiles_count) { 1 }
+        Bebox::Role.stub(:roles_count) { 1 }
+        Bebox::Node.stub(:count_all_nodes_by_type) { 1 }
+        Bebox::NodeWizard.any_instance.stub(:send) { true }
+        argv = ['node', 'set_role']
+        capture(:stdout) { cli_command(argv, :success) }
       end
 
       it 'creates a new node' do
@@ -116,6 +143,88 @@ describe 'Test 00: Bebox::Cli' do
       it 'removes a node' do
         Bebox::EnvironmentWizard.any_instance.stub(:send) { true }
         argv = ['node', 'remove']
+        capture(:stdout) { cli_command(argv, :success) }
+      end
+    end
+
+    context '04: prepare commands' do
+
+      before :each do
+        Bebox::Node.stub(:count_all_nodes_by_type) { 1 }
+        Bebox::Environment.stub(:list) { [node.environment] }
+        Bebox::Node.stub(:list) { [node] }
+        Bebox::Node.stub(:nodes_in_environment) { [node] }
+      end
+
+      it 'shows an error if vagrant is not installed' do
+        Bebox::CommandsHelper.stub(:vagrant_installed?) { false }
+        argv = ['prepare']
+        output = capture(:stdout) { cli_command(argv, :success) }
+        expect(output).to match(/Vagrant is not installed in the system. No changes were made./m)
+      end
+
+      it 'prepares a node' do
+        Bebox::NodeWizard.any_instance.stub(:prepare) { true }
+        argv = ['prepare']
+        output = capture(:stdout) { cli_command(argv, :success) }
+      end
+
+      it 'halts a vagrant node' do
+        Bebox::VagrantHelper.stub(:send) { true }
+        argv = ['vagrant_halt']
+        capture(:stdout) { cli_command(argv, :success) }
+      end
+
+      it 'ups a vagrant node' do
+        Bebox::VagrantHelper.stub(:send) { true }
+        argv = ['vagrant_up']
+        capture(:stdout) { cli_command(argv, :success) }
+      end
+    end
+
+    context '05: profile commands' do
+
+      before :each do
+        Bebox::Node.stub(:count_all_nodes_by_type) { 1 }
+        Bebox::Environment.stub(:list) { [node.environment] }
+      end
+
+      it 'shows the help for node commands' do
+        argv = ['help', 'profile']
+        output = capture(:stdout) { cli_command(argv, :success) }
+        expected_content = File.read("spec/fixtures/commands/profile_help.test").gsub(/\s+/, ' ').strip
+        expect(output.gsub(/\s+/, ' ').strip).to eq(expected_content)
+      end
+
+      it 'list profiles if there are any' do
+        Bebox::ProfileWizard.any_instance.stub(:list_profiles) { [profile.name] }
+        argv = ['profile', 'list']
+        output = capture(:stdout) { cli_command(argv, :success) }
+        expect(output).to match(/Current profiles:.*?#{profile.name}/m)
+      end
+
+      it 'not list nodes if there are not any' do
+        Bebox::ProfileWizard.any_instance.stub(:list_profiles) { [] }
+        argv = ['profile', 'list']
+        output = capture(:stdout) { cli_command(argv, :success) }
+        expect(output).to match(/There are not profiles yet. You can create a new one with: 'bebox profile new' command./m)
+      end
+
+      it 'can not create a new profile without name' do
+        argv = ['profile', 'new']
+        output = capture(:stdout) { cli_command(argv, :failure) }
+        expect(output).to match(/You did not supply a name/)
+      end
+
+      it 'creates a new profile with name' do
+        Bebox::ProfileWizard.any_instance.stub(:create_new_profile) { true }
+        argv = ['profile', 'new', profile.name]
+        capture(:stdout) { cli_command(argv, :success) }
+      end
+
+      it 'removes a profile' do
+        Bebox::ProfileWizard.any_instance.stub(:remove_profile) { true }
+        argv = ['profile', 'remove', profile.name]
         capture(:stdout) { cli_command(argv, :success) }
       end
     end
